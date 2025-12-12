290a61c92ba02d57d2b28dce8f17f7ef0213c242
elamvaluthikowsalya-cloud <elamvaluthikowsalya@gmail.com>
2025-12-12 11:36:11 +0530

    first time

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from "react";
+class App extends React.Component
+{
+  render()
+   {
+      return(<>
+       <h2>Welcome to React</h2>
+       <h2>Hello</h2>
+       <h2>Welcome to our Marriage</h2>
+       <h2>Manoj Weds Sneha</h2>
+       <h2>Invite You All</h2>
+       <p>Amet duis veniam exercitation aliquip eiusmod. Irure elit ex esse sunt proident sunt in. Laboris excepteur ea anim anim laborum. Adipisicing id consequat do ad consequat dolor Lorem. Labore aliqua officia ullamco cillum nisi tempor sunt ipsum cupidatat.
+
+Laborum aute ad magna nostrud amet reprehenderit enim voluptate aliquip esse nisi. Consectetur ullamco ut in consequat magna adipisicing sunt cupidatat commodo. Ullamco irure pariatur nisi velit esse elit. Esse velit elit aute nostrud nostrud mollit sit in aliquip Lorem id tempor reprehenderit dolore.-</p>
+       </>)
+       }
+}
+export default App
+/*import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+*/
